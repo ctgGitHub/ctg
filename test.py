1ef6ab3de3f9f7d5d93c46d76b196712ec59ddf6
ctgGitHub <39004307+ctgGitHub@users.noreply.github.com>
2018-05-06 22:19:34 +0800

    my python
    test github

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+val = 0
+def test2():
+	global val
+	print "test begin"
+	while val < 12:
+		val = val + 1
+		print val
+	print "test end"	
+	
+test2()
+def test1( b ):
+	print b
+test1(val)	
